d5bf6f57cd18d20d9224ca04e9beccbd7c981ed9
AndrewParking <popow.andrej2009@yandex.ru>
2015-08-16 11:05:56 +0300

    Initial commit

---
+++ lsd/settings/dev.py
@@ -0,0 +1,11 @@
+from .base import *
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DEBUG = True
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
